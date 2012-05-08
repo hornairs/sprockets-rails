3d683e3b0508f1a670cd9a98fe602286ae90ed94
Harry Brundage <harry.brundage@gmail.com>
2012-05-08 17:29:49 -0500

    Monkey patch Rails::Application to have an accessor for :assets, where the Sprockets environment is stored.

--- lib/sprockets-rails.rb
+++ lib/sprockets-rails.rb
@@ -1,6 +1,7 @@
 require 'sprockets'
 require 'sprockets/rails/railtie'
 require 'sprockets/extensions/rails/application_configuration'
+require 'sprockets/extensions/rails/application'
 
 module Sprockets
   module Rails
