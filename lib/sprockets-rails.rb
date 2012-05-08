4efb78ccff8dc26233480dcadf8e2a3b754ba6c4
Harry Brundage <harry.brundage@gmail.com>
2012-05-08 17:30:25 -0500

    Import Rails 3.1 ActionView::AssetPaths which is used by the RailsHelpers as a base class.

--- lib/sprockets-rails.rb
+++ lib/sprockets-rails.rb
@@ -2,6 +2,7 @@ require 'sprockets'
 require 'sprockets/rails/railtie'
 require 'sprockets/extensions/rails/application_configuration'
 require 'sprockets/extensions/rails/application'
+require 'sprockets/extensions/rails/action_view/asset_paths'
 
 module Sprockets
   module Rails
